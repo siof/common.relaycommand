5942d9a947e103c4cdfaeae32f854fda6f1d0c8b
siof <siof@siof.pl>
2017-10-18 18:47:04 +0100

    init

---
+++ src/siof.Common.RelayCommand/RelayCommandGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace siof.Common
+{
+    public class RelayCommand: RelayCommand<object>
+    {
+        public RelayCommand(Action<object> execute) :
+            base(execute)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute) :
+            base(execute, canExecute)
+        {
+        }
+    }
+}
